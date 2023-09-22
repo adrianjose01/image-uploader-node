1b7b84cafb6c013ab3679c96480017f24605681b
adrianjose01 <dejesuseusebbio21@hotmail.com>
2023-09-22 02:28:34 -0400

    image-uploader-backend

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const multer = require("multer");
+const cors = require("cors");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+let imageUrlPath;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./Images");
+  },
+
+  filename: (req, file, cb) => {
+    console.log(file);
+    imageUrlPath = `${Date.now().toString().slice(0, 6)}_${file.originalname}`;
+    cb(null, `${Date.now().toString().slice(0, 6)}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, req.originalUrl));
+});
+
+app.post("/upload-image", upload.single("file"), (req, res, next) => {
+  res.json({
+    imagePath: req.protocol + "://" + "localhost:4000" + "/" + req.file.path,
+  });
+});
+
+app.listen(4000, () => {
+  console.log("Server running in Port 4000");
+});
